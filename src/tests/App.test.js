d6a77d9c1fbbae6d13383c72aac59e97a7663c7a
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-08-18 15:48:25 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+describe('testando links', () => {
+  test('testando o link "Home"', () => {
+    renderWithRouter(<App />);
+
+    const homeButton = screen.getByRole('link', { name: 'Home' });
+    expect(homeButton).toBeInTheDocument();
+  });
+
+  test('testanto o link "About"', () => {
+    renderWithRouter(<App />);
+    const aboutText = screen.getByRole('link', { name: 'About' });
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('testando o link "Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const favPokeText = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favPokeText).toBeInTheDocument();
+  });
+});
