a210e7b9aa532481e229d244dca4ae3ae7c8a978
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-08-18 17:30:48 -0300

    requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+const POKE_ID = 'pokemon-name';
+describe('testa o component Pokedex', () => {
+  test('testa se existe o texto "Encoutered pokémons"', () => {
+    renderWithRouter(<App />);
+    const findPokesText = screen.getByText('Encountered pokémons');
+    expect(findPokesText).toBeInTheDocument();
+  });
+
+  test('testa se existe um botão com texto "Próximo pokémon"', () => {
+    renderWithRouter(<App />);
+    const nextPokeButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(nextPokeButton).toBeInTheDocument();
+  });
+
+  test('testa se existe 7 botões de filtro', () => {
+    renderWithRouter(<App />);
+    const typePokeButton = screen.getAllByTestId('pokemon-type-button');
+    const buttonsNumber = 7;
+    expect(typePokeButton.length).toBe(buttonsNumber);
+    const typePoke = screen.getByRole('button', { name: 'Electric' });
+    expect(typePoke).toBeInTheDocument();
+    userEvent.click(typePoke);
+    const pokemonActual = screen.getByTestId(POKE_ID);
+    expect(pokemonActual.textContent).toContain('Pikachu');
+  });
+
+  test('testa se o botão all existe', () => {
+    renderWithRouter(<App />);
+    const allButton = screen.getByText('All');
+    expect(allButton).toBeInTheDocument();
+  });
+
+  test('testa se o filtro funciona', () => {
+    renderWithRouter(<App />);
+    const allButton = screen.getByText('All');
+    expect(allButton).toBeInTheDocument();
+    const psychicButton = screen.getByText('Psychic');
+    userEvent.click(psychicButton);
+    const pokemonActualPsy = screen.getByTestId(POKE_ID);
+    expect(pokemonActualPsy.textContent).toContain('Alakazam');
+    userEvent.click(allButton);
+    const pokemonActual = screen.getByTestId(POKE_ID);
+    expect(pokemonActual.textContent).toContain('Pikachu');
+  });
+});
