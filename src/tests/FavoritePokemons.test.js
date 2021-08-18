5f2b9b7bb24c971d1eccf3bb3237650418a55591
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-08-18 16:20:01 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+describe('testa o component FavoritePokemons', () => {
+  test('testando se existe o texto "No favorite pokemon found"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const pokeNotFound = screen.getByText('No favorite pokemon found');
+    expect(pokeNotFound).toBeInTheDocument();
+  });
+});
